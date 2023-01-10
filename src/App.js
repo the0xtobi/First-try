9c76edcd6cc9cf60cfd70cf0a2edc5d5dbff7b43
blur <thetobiterra@gmail.com>
2023-01-10 06:01:22 +0100

    minors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react"
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
@@ -8,6 +7,7 @@ function App() {
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,3 @@
-import React from "react"
-
 export default function Footer(){
     return(
         <h2>This is my footer</h2>
